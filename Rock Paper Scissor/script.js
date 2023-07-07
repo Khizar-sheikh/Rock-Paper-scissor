7dfdf543b57babc9f6483a98616e6caff12be615
Khizar Sheikh <khizarsheikh.012@gmail.com>
2023-07-07 21:28:33 +0500

    Comment the whole cause made a new one

--- Rock Paper Scissor/script.js
+++ Rock Paper Scissor/script.js
@@ -1,46 +1,38 @@
 
-function getComputerChoice(){
-    const Computer = ['ROCK','PAPER','SCISSOR']
-    const randomNumber = Math.floor(Math.random() * Computer.length);
-    return Computer[randomNumber]
-}
+// Old Javascript
+// function getComputerChoice(){
+//     const Computer = ['ROCK','PAPER','SCISSOR']
+//     const randomNumber = Math.floor(Math.random() * Computer.length);
+//     return Computer[randomNumber]
+// }
+// function playround(playerSelection, computerSelection){
+//     let playerChoice=playerSelection.toLowerCase()
+//      let computerChoice=computerSelection.toLowerCase()
+//      if (playerChoice === computerChoice)
+//      return `"Tie!" Yourchoice and Computerchoice are same.`
+//      else if(!(playerChoice=== "rock" &&"paper" &&"scissor")){
+//       return "You made an invalid choice "
+//      }
+//      else if( (playerChoice==="rock" && computerChoice ==="scissor")||
+//      (playerChoice==="paper" && computerChoice==="rock" )||
+//      (playerChoice==="scissor"&& computerChoice==="paper")
+//      ){
+//         return `"You Win!" ${playerChoice} beats ${computerChoice} `
+//      }
+//      else  {
+//        return `"You Lose!" ${computerChoice} beats ${playerChoice} `
+//      } 
+// }
+// function game() {
+//    for (let i = 0; i < 5; i++) {
+//      let playerSelection = prompt("Make a choice: ");
+//      let computerSelection = getComputerChoice();
+//      console.log(playround(playerSelection, computerSelection));
+//    }
+//    console.log("Game ended!");
+//  }
+// game()
 
-function playround(playerSelection, computerSelection){
-    let playerChoice=playerSelection.toLowerCase()
-     let computerChoice=computerSelection.toLowerCase()
-     if (playerChoice === computerChoice)
-     return `"Tie!" yourchoice and computerchoice are same.`
-     else if(!(playerChoice=== "rock" &&"paper" &&"scissor")){
-      return "You made an invalid choice "
-     }
-     else if( (playerChoice==="rock" && computerChoice ==="scissor")||
-     (playerChoice==="paper" && computerChoice==="rock" )||
-     (playerChoice==="scissor"&& computerChoice==="paper")
-     ){
-        return `"You Win!" ${playerChoice} beats ${computerChoice} `
-     }
-     else  {
-       return `"You Lose!" ${computerChoice} beats ${playerChoice} `
-     }
-}
-function game(){
-   
-   let playerSelection=prompt("Make  a choice ");
 
-   let computerSelection= getComputerChoice();
-   console.log(playround(playerSelection, computerSelection)); 
-   playerSelection=prompt("Make  a choice ");
-   console.log(playround(playerSelection, computerSelection));   
-   playerSelection=prompt("Make  a choice "); 
-   console.log(playround(playerSelection, computerSelection));    
-   playerSelection=prompt("Make  a choice ");
-   console.log(playround(playerSelection, computerSelection));    
-   playerSelection=prompt("Make  a choice ");
-   console.log(playround(playerSelection, computerSelection));
-   
-   console.log("Game Ended!")
-   
-}
-game()
 
 
